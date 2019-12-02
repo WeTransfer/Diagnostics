2573e187927941dae0d60694e9f5dfa48623bb0a
Antoine van der Lee <antoine@wetransfer.com>
2019-12-02 11:55:48 +0100

    Initial project setup

---
+++ Diagnostics/Diagnostics.h
@@ -0,0 +1,19 @@
+//
+//  Diagnostics.h
+//  Diagnostics
+//
+//  Created by Antoine van der Lee on 02/12/2019.
+//  Copyright © 2019 WeTransfer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Diagnostics.
+FOUNDATION_EXPORT double DiagnosticsVersionNumber;
+
+//! Project version string for Diagnostics.
+FOUNDATION_EXPORT const unsigned char DiagnosticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Diagnostics/PublicHeader.h>
+
+
