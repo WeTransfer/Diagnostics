524a7e07427125b84ba9249fcf1f456dd26d9daa
Antoine van der Lee <antoine@wetransfer.com>
2019-12-06 16:28:59 +0100

    Re-enable tests

--- DiagnosticsTests/DiagnosticsReporterTests.swift
+++ DiagnosticsTests/DiagnosticsReporterTests.swift
@@ -21,37 +21,37 @@ final class DiagnosticsReporterTests: XCTestCase {
         super.tearDown()
     }
 
-//    /// It should correctly generate HTML from the reporters.
-//    func testHTMLGeneration() {
-//        let diagnosticsChapter = DiagnosticsChapter(title: UUID().uuidString, diagnostics: UUID().uuidString)
-//        MockedReporter.diagnosticsChapter = diagnosticsChapter
-//        let reporters = [MockedReporter.self]
-//        let report = DiagnosticsReporter.create(using: reporters)
-//        let html = String(data: report.data, encoding: .utf8)!
-//
-//        XCTAssertTrue(html.contains("<h3>\(diagnosticsChapter.title)</h3>"))
-//        XCTAssertTrue(html.contains(diagnosticsChapter.diagnostics as! String))
-//    }
-//
-//    /// It should create a chapter for each reporter.
-//    func testReportingChapters() {
-//        let report = DiagnosticsReporter.create()
-//        let html = String(data: report.data, encoding: .utf8)!
-//        let expectedChaptersCount = DiagnosticsReporter.DefaultReporter.allCases.count
-//        let chaptersCount = html.components(separatedBy: "<div class=\"chapter\"").count - 1
-//        XCTAssertEqual(expectedChaptersCount, chaptersCount)
-//    }
-//
-//    /// It should correctly generate the header.
-//    func testHeaderGeneration() {
-//        let report = DiagnosticsReporter.create(using: [])
-//        let html = String(data: report.data, encoding: .utf8)!
-//
-//        XCTAssertTrue(html.contains("<head>"))
-//        XCTAssertTrue(html.contains("<title>\(DiagnosticsReporter.reportTitle)</title>"))
-//        XCTAssertTrue(html.contains(DiagnosticsReporter.style()))
-//        XCTAssertTrue(html.contains("</head>"))
-//    }
+    /// It should correctly generate HTML from the reporters.
+    func testHTMLGeneration() {
+        let diagnosticsChapter = DiagnosticsChapter(title: UUID().uuidString, diagnostics: UUID().uuidString)
+        MockedReporter.diagnosticsChapter = diagnosticsChapter
+        let reporters = [MockedReporter.self]
+        let report = DiagnosticsReporter.create(using: reporters)
+        let html = String(data: report.data, encoding: .utf8)!
+
+        XCTAssertTrue(html.contains("<h3>\(diagnosticsChapter.title)</h3>"))
+        XCTAssertTrue(html.contains(diagnosticsChapter.diagnostics as! String))
+    }
+
+    /// It should create a chapter for each reporter.
+    func testReportingChapters() {
+        let report = DiagnosticsReporter.create()
+        let html = String(data: report.data, encoding: .utf8)!
+        let expectedChaptersCount = DiagnosticsReporter.DefaultReporter.allCases.count
+        let chaptersCount = html.components(separatedBy: "<div class=\"chapter\"").count - 1
+        XCTAssertEqual(expectedChaptersCount, chaptersCount)
+    }
+
+    /// It should correctly generate the header.
+    func testHeaderGeneration() {
+        let report = DiagnosticsReporter.create(using: [])
+        let html = String(data: report.data, encoding: .utf8)!
+
+        XCTAssertTrue(html.contains("<head>"))
+        XCTAssertTrue(html.contains("<title>\(DiagnosticsReporter.reportTitle)</title>"))
+        XCTAssertTrue(html.contains(DiagnosticsReporter.style()))
+        XCTAssertTrue(html.contains("</head>"))
+    }
 
     /// It should minify CSS.
     func testCSSMinify() {
