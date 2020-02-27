91da4fa07de046e6b2e66938b1ad01512cf24229
David Steppenbeck <steppenbeck@gmail.com>
2020-02-27 12:41:10 +0000

    Add HTML encoding to logs in final report.

---
+++ Sources/HTMLEncoding.swift
@@ -0,0 +1,22 @@
+//
+//  HTMLEncoding.swift
+//  Diagnostics
+//
+//  Created by David Steppenbeck on 2020/02/26.
+//  Copyright © 2020 WeTransfer. All rights reserved.
+//
+
+import Foundation
+
+public protocol HTMLEncoding {
+    func addingHTMLEncoding() -> HTML
+}
+
+extension String: HTMLEncoding {
+    
+    /// Encodes entities to be displayed correctly in the final HTML report.
+    public func addingHTMLEncoding() -> HTML {
+        return self.replacingOccurrences(of: "<", with: "&lt;")
+                   .replacingOccurrences(of: ">", with: "&gt;")
+    }
+}
