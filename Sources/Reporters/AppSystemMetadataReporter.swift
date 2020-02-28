a9539486b0990c5e6b5e1e8f76138ed96a1b0e30
David Steppenbeck <steppenbeck@gmail.com>
2020-02-28 14:24:43 +0000

    Trivial fix.

--- Sources/Reporters/AppSystemMetadataReporter.swift
+++ Sources/Reporters/AppSystemMetadataReporter.swift
@@ -22,29 +22,29 @@ public struct AppSystemMetadataReporter: DiagnosticsReporting {
         case appLanguage = "App Language"
     }
 
-    static let hardwareName: [String : String] = [
-        "iPhone7,1"  : "iPhone 6 Plus",
-        "iPhone7,2"  : "iPhone 6",
-        "iPhone8,1"  : "iPhone 6s",
-        "iPhone8,2"  : "iPhone 6s Plus",
-        "iPhone8,4"  : "iPhone SE",
-        "iPhone9,1"  : "iPhone 7",
-        "iPhone9,2"  : "iPhone 7 Plus",
-        "iPhone9,3"  : "iPhone 7",
-        "iPhone9,4"  : "iPhone 7 Plus",
-        "iPhone10,1" : "iPhone 8",
-        "iPhone10,2" : "iPhone 8 Plus",
-        "iPhone10,3" : "iPhone X",
-        "iPhone10,4" : "iPhone 8",
-        "iPhone10,5" : "iPhone 8 Plus",
-        "iPhone10,6" : "iPhone X",
-        "iPhone11,2" : "iPhone XS",
-        "iPhone11,4" : "iPhone XS Max",
-        "iPhone11,6" : "iPhone XS Max",
-        "iPhone11,8" : "iPhone XR",
-        "iPhone12,1" : "iPhone 11",
-        "iPhone12,3" : "iPhone 11 Pro",
-        "iPhone12,5" : "iPhone 11 Pro Max"
+    static let hardwareName: [String: String] = [
+        "iPhone7,1": "iPhone 6 Plus",
+        "iPhone7,2": "iPhone 6",
+        "iPhone8,1": "iPhone 6s",
+        "iPhone8,2": "iPhone 6s Plus",
+        "iPhone8,4": "iPhone SE",
+        "iPhone9,1": "iPhone 7",
+        "iPhone9,2": "iPhone 7 Plus",
+        "iPhone9,3": "iPhone 7",
+        "iPhone9,4": "iPhone 7 Plus",
+        "iPhone10,1": "iPhone 8",
+        "iPhone10,2": "iPhone 8 Plus",
+        "iPhone10,3": "iPhone X",
+        "iPhone10,4": "iPhone 8",
+        "iPhone10,5": "iPhone 8 Plus",
+        "iPhone10,6": "iPhone X",
+        "iPhone11,2": "iPhone XS",
+        "iPhone11,4": "iPhone XS Max",
+        "iPhone11,6": "iPhone XS Max",
+        "iPhone11,8": "iPhone XR",
+        "iPhone12,1": "iPhone 11",
+        "iPhone12,3": "iPhone 11 Pro",
+        "iPhone12,5": "iPhone 11 Pro Max"
     ]
 
     static var title: String = "App & System Details"
