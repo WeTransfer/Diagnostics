bf3781604c38dcc61aeda07934c8dce07be42ad5
Antoine van der Lee <ajvanderlee@gmail.com>
2021-04-12 15:50:58 +0200

    Package is now static and uses swift tools 5.3 (#81)
    * Package is now static and uses swift tools 5.3
    
    * Fix Danger reference
    
    * Remove trailing komma

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 // We're hiding dev, test, and danger dependencies with // dev to make sure they're not fetched by users of this package.
 
@@ -12,14 +12,14 @@ let package = Package(name: "Diagnostics",
                         .watchOS(.v6)],
                       products: [
                         // dev .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]),
-                        .library(name: "Diagnostics", targets: ["Diagnostics"])
+                        .library(name: "Diagnostics", type: .static, targets: ["Diagnostics"])
                         ],
                       dependencies: [
-                        // dev .package(url: "https://github.com/danger/swift", from: "3.0.0"),
-                        // dev .package(path: "Submodules/WeTransfer-iOS-CI/Danger-Swift")
+                        // dev .package(name: "danger-swift", url: "https://github.com/danger/swift", from: "3.0.0"),
+                        // dev .package(name: "WeTransferPRLinter", path: "Submodules/WeTransfer-iOS-CI/Danger-Swift")
                         ],
                       targets: [
-                        // dev .target(name: "DangerDependencies", dependencies: ["Danger", "WeTransferPRLinter"], path: "Submodules/WeTransfer-iOS-CI/Danger-Swift", sources: ["DangerFakeSource.swift"]),
-                        .target(name: "Diagnostics", path: "Sources")
+                        // dev .target(name: "DangerDependencies", dependencies: [.product(name: "Danger", package: "danger-swift"), "WeTransferPRLinter"], path: "Submodules/WeTransfer-iOS-CI/Danger-Swift", sources: ["DangerFakeSource.swift"]),
+                        .target(name: "Diagnostics", path: "Sources", exclude: ["style.css"])
                         ],
                       swiftLanguageVersions: [.v5])
