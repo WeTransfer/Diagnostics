a21b0069cf839e3ec84e3db556935f61f4bfb56f
Antoine van der Lee <ajvanderlee@gmail.com>
2021-10-12 18:00:54 +0200

    Go back to Swift 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 // We're hiding dev, test, and danger dependencies with // dev to make sure they're not fetched by users of this package.
 
