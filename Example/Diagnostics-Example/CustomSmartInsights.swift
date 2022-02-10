30c992cf940ef1b8116ed0cd15f3be3844f55c97
Antoine van der Lee <ajvanderlee@gmail.com>
2022-02-10 15:22:37 +0100

    Update readme

--- Example/Diagnostics-Example/CustomSmartInsights.swift
+++ Example/Diagnostics-Example/CustomSmartInsights.swift
@@ -12,7 +12,12 @@ struct SmartInsightsProvider: SmartInsightsProviding {
     func smartInsights(for chapter: DiagnosticsChapter) -> [SmartInsightProviding]? {
         guard let html = chapter.diagnostics as? HTML else { return nil }
         if html.errorLogs.contains(where: { $0.contains("AppDelegate.ExampleLocalizedError") }) {
-            return [SmartInsight(name: "Localized data", result: .warn(message: "An error was found regarding missing localisation."))]
+            return [
+                SmartInsight(
+                    name: "Localized data",
+                    result: .warn(message: "An error was found regarding missing localisation.")
+                )
+            ]
         }
         return nil
     }
