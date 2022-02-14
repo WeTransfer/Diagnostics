76ddc3c5a40b3870abb9b245bb37de0e346cdbd9
Antoine van der Lee <ajvanderlee@gmail.com>
2022-02-14 14:07:28 +0100

    Fix CI

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-// We're hiding dev, test, and danger dependencies with // dev to make sure they're not fetched by users of this package.
 
 import PackageDescription
 
@@ -11,15 +10,9 @@ let package = Package(name: "Diagnostics",
                         .tvOS(.v12),
                         .watchOS(.v6)],
                       products: [
-                        // dev .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]),
                         .library(name: "Diagnostics", type: .static, targets: ["Diagnostics"])
                         ],
-                      dependencies: [
-                        // dev .package(name: "danger-swift", url: "https://github.com/danger/swift", from: "3.12.1"),
-                        // dev .package(name: "WeTransferPRLinter", path: "Submodules/WeTransfer-iOS-CI/WeTransferPRLinter")
-                        ],
                       targets: [
-                        // dev .target(name: "DangerDependencies", dependencies: [.product(name: "Danger", package: "danger-swift"), .product(name: "WeTransferPRLinter", package: "WeTransferPRLinter")], path: "Submodules/WeTransfer-iOS-CI/DangerFakeSources", sources: ["DangerFakeSource.swift"]),
                         .target(
                             name: "Diagnostics",
                             path: "Sources",
