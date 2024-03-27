d6aa786d5d508abe06302e7ecdf8c381c7451841
Rubén Fernández <contacto@rubenfernandez.dev>
2024-03-27 17:37:25 +0100

    feat: add visionOS support

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,10 +6,9 @@ import PackageDescription
 let package = Package(name: "Diagnostics",
                       platforms: [
                         .macOS(.v10_15),
-                        .iOS(.v12),
+                        .iOS(.v11),
                         .tvOS(.v12),
-                        .watchOS(.v6),
-                        .visionOS(.v1)],
+                        .watchOS(.v6)],
                       products: [
                         .library(name: "Diagnostics", targets: ["Diagnostics"])
                         ],
